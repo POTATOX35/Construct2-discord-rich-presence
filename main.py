bc7f238db9bbee912132bfc5632c8233710b883a
POTATOX <81747015+POTATOX35@users.noreply.github.com>
2023-03-30 16:42:12 +0300

    v1.0

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from pypresence import Presence
 import pyautogui
 from functions import *
 
-flag = 0
+import wmi
+  
+
+f = wmi.WMI()
+  
+
+
 client_id ="1074262494740230164"
 RPC = Presence(client_id)
 RPC.connect()
 lastname = ''
-while True:
+flag =0    
     
-    if ("Construct2.exe" in (i.name() for i in psutil.process_iter())) == True:
+for process in f.Win32_Process():
+    if "Construct2.exe" == process.Name:
         if (lastname != project_name):
             RPC.connect()
             lastname = project_name
@@ -21,7 +28,7 @@ while True:
             large_text="Playing Construct 2",
             start=int(time.time())
         )
-    else:
-        RPC.clear()
-    
-    
+        break
+  
+if flag == 0:
+    RPC.clear()
